c62df573c654ba4cfbc2b7787c6f6a54b50ed06f
ahsanulks <ahsanulkh996@gmail.com>
2024-01-23 10:28:32 +0000

    init app

---
+++ src/main/java/com/ahsanulks/moneyforward/MoneyforwardApplication.java
@@ -0,0 +1,13 @@
+package com.ahsanulks.moneyforward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyforwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyforwardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahsanulks/moneyforward/MoneyforwardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahsanulks.moneyforward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyforwardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

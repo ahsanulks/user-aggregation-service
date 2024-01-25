602c3a895d74f3e42fceddd95cd7ef826c20164b
ahsanulks <ahsanulkh996@gmail.com>
2024-01-25 09:45:45 +0000

    feat: refactor mapping user response

--- src/main/java/com/ahsanulks/moneyforward/hexagon/ports/driven/UserResponseDto.java
+++ src/main/java/com/ahsanulks/moneyforward/hexagon/ports/driven/UserResponseDto.java
@@ -2,13 +2,50 @@ package com.ahsanulks.moneyforward.hexagon.ports.driven;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
 @Setter
 public class UserResponseDto {
+    private UserAttribute attributes;
+
+    public UserResponseDto() {
+        this.attributes = new UserAttribute();
+    }
+
+    public int getId() {
+        return this.attributes.getId();
+    }
+
+    public String getName() {
+        return this.attributes.getName();
+    }
+
+    public List<Integer> getAccountIds() {
+        return this.attributes.getAccountIds();
+    }
+
+    public void setId(int id) {
+        this.attributes.setId(id);
+    }
+
+    public void setName(String name) {
+        this.attributes.setName(name);
+    }
+
+    public void setAccountIds(List<Integer> accountIds) {
+        this.attributes.setAccountIds(accountIds);
+    }
+}
+
+@Setter
+@Getter
+class UserAttribute {
     private int id;
     private String name;
+
+    @JsonProperty("account_ids")
     private List<Integer> accountIds;
 }

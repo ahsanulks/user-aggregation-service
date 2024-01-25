d09647f91463131c2221c57ea885257d5360ae6d
ahsanulks <ahsanulkh996@gmail.com>
2024-01-25 08:02:22 +0000

    feat: add implementation get user accounts

--- src/main/java/com/ahsanulks/moneyforward/adapter/driven/UserApiAdapter.java
+++ src/main/java/com/ahsanulks/moneyforward/adapter/driven/UserApiAdapter.java
@@ -1,17 +1,26 @@
 package com.ahsanulks.moneyforward.adapter.driven;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.ahsanulks.moneyforward.hexagon.ports.driven.AccountResponseDto;
 import com.ahsanulks.moneyforward.hexagon.ports.driven.UserPort;
 import com.ahsanulks.moneyforward.hexagon.ports.driven.UserResponseDto;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class UserApiAdapter implements UserPort {
 
@@ -30,7 +39,20 @@ public class UserApiAdapter implements UserPort {
 
     @Override
     public List<AccountResponseDto> getUserAccounts(int id) {
-        return new ArrayList<>();
+        try {
+            ResponseEntity<List<AccountResponseDto>> responseEntity = restTemplate.exchange(
+                    userBaseUrl + "/users/{id}/accounts",
+                    HttpMethod.valueOf("GET"),
+                    null,
+                    new ParameterizedTypeReference<List<AccountResponseDto>>() {
+                    },
+                    id);
+
+            return responseEntity.getBody();
+        } catch (RestClientException ex) {
+            log.error("Error while calling user API: {}", ex.getMessage(), ex);
+            throw ex;
+        }
     }
 
 }
---
+++ src/main/java/com/ahsanulks/moneyforward/config/RestTemplateConfig.java
@@ -0,0 +1,14 @@
+package com.ahsanulks.moneyforward.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}

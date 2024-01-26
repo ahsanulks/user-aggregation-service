eef8fb0b3479a81d5d8478fd9950bdc91dfdfdf7
ahsanulks <ahsanulkh996@gmail.com>
2024-01-26 07:16:10 +0000

    feat: add implementation user found

--- src/main/java/com/ahsanulks/moneyforward/adapter/driver/UserController.java
+++ src/main/java/com/ahsanulks/moneyforward/adapter/driver/UserController.java
@@ -1,7 +1,15 @@
 package com.ahsanulks.moneyforward.adapter.driver;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ahsanulks.moneyforward.hexagon.ports.driver.GetUserService;
 
+@RestController
+@RequestMapping("api/v1")
 public class UserController {
     private final GetUserService getUserService;
 
@@ -9,4 +17,9 @@ public class UserController {
         this.getUserService = getUserService;
     }
 
+    @GetMapping("users/{id}")
+    public ResponseEntity<Object> getUserById(@PathVariable int id) {
+        var user = getUserService.getUserAccountById(id);
+        return ResponseEntity.ok().body(user);
+    }
 }

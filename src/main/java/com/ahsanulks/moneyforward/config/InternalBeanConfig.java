eed6f48dfe67558169281146478c450508b29f0c
ahsanulks <ahsanulkh996@gmail.com>
2024-01-29 09:19:12 +0000

    feat: add config to toggle decorator

--- src/main/java/com/ahsanulks/moneyforward/config/InternalBeanConfig.java
+++ src/main/java/com/ahsanulks/moneyforward/config/InternalBeanConfig.java
@@ -1,15 +1,26 @@
 package com.ahsanulks.moneyforward.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.ahsanulks.moneyforward.hexagon.internal.UserGetterDecorator;
 import com.ahsanulks.moneyforward.hexagon.internal.UserGetterService;
+import com.ahsanulks.moneyforward.hexagon.ports.driven.AccountPort;
 import com.ahsanulks.moneyforward.hexagon.ports.driven.UserPort;
+import com.ahsanulks.moneyforward.hexagon.ports.driver.GetUserService;
 
 @Configuration
 public class InternalBeanConfig {
+    @Value("${user.service.decorator.enabled}")
+    private boolean userServiceDecoratorEnabled;
+
     @Bean
-    UserGetterService userGetterService(UserPort userPort) {
-        return new UserGetterService(userPort);
+    GetUserService userGetterService(UserPort userPort, AccountPort accountPort) {
+        GetUserService userService = new UserGetterService(userPort);
+        if (userServiceDecoratorEnabled) {
+            userService = new UserGetterDecorator(userService, accountPort);
+        }
+        return userService;
     }
 }

d12c5ed22278ba8cd459d1ee759deaf1859e29f4
ahsanulks <ahsanulkh996@gmail.com>
2024-01-29 09:03:59 +0000

    feat: handle call to account

--- src/main/java/com/ahsanulks/moneyforward/adapter/driven/AccountApiAdapter.java
+++ src/main/java/com/ahsanulks/moneyforward/adapter/driven/AccountApiAdapter.java
@@ -4,11 +4,18 @@ import java.math.BigDecimal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.ahsanulks.moneyforward.hexagon.ports.driven.AccountPort;
+import com.ahsanulks.moneyforward.hexagon.ports.driven.AccountResponseDto;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class AccountApiAdapter implements AccountPort {
 
@@ -22,7 +29,24 @@ public class AccountApiAdapter implements AccountPort {
 
     @Override
     public Optional<BigDecimal> getAccountBalance(int id) {
-        return Optional.empty();
+        log.info("Call Account API with id {}", id);
+
+        try {
+            var account = restTemplate.getForObject(accountBaseUrl + "/accounts/{id}", AccountResponseDto.class, id);
+            if (account == null) {
+                return Optional.empty();
+            }
+            return Optional.ofNullable(account.getBalance());
+        } catch (RestClientException ex) {
+            log.error("Error while calling account API: {}", ex.getMessage(), ex);
+
+            if (ex instanceof HttpClientErrorException
+                    && ((HttpClientErrorException) ex).getStatusCode() == HttpStatus.NOT_FOUND) {
+                return Optional.empty();
+            } else {
+                throw new RuntimeException("Internal Server Error", ex);
+            }
+        }
     }
 
 }
